f001c1fafece7fed193a1d03ade6530170f0fe17
melinamerli <melinamcf88@gmail.com>
2023-08-04 21:41:26 -0300

    cambios a php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+  $pg = "inicio";
+?>
+
 <!DOCTYPE html>
 <html lang="es" class="h-100">
 <head>
@@ -12,34 +16,12 @@
 </head>
 <body id="inicio">
     <header class="container">
-        <nav class="navbar navbar-expand-md mb-4">
-            <div class="container-fluid">
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                  <li class="nav-item">
-                    <a class="nav-link active  px-4" href="index.html">Inicio</a>
-                  </li>
-                  <li class="nav-item ">
-                    <a class="nav-link  px-4" href="sobre-mi.html">Sobre mi</a>
-                  </li>
-                  <li class="nav-item ">
-                    <a class="nav-link px-4" href="proyectos.html">Proyectos</a>
-                  </li>
-                  <li class="nav-item ">
-                    <a class="nav-link px-4" href="contacto.html">Contacto</a> <!--active indica cual es el menu activo-->
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
+        <?php include_once "menu.php";?>
     </header>
     <main class="container">
       <div class="row">
         <div class="col-12 text-center mt-5 cohete">
-          <a href="proyectos.html"><img src="../portfolio/images/cohete.svg"></a>
+          <a href="proyectos.php"><img src="../portfolio/images/cohete.svg"></a>
         </div>
       </div>
       <div class="row">
@@ -49,7 +31,7 @@
       </div>
       <div class="row">
         <div class="col-12 text-center">
-        <a href="proyectos.html" class="btn btn-blanco">Conoce mis proyectos</a>
+        <a href="proyectos.php" class="btn btn-blanco">Conoce mis proyectos</a>
       </div>
       </div>
     </main>
--- sobre-mi.php
+++ sobre-mi.php
@@ -1,3 +1,7 @@
+<?php
+  $pg = "sobre-mi";
+?>
+
 <!DOCTYPE html>
 <html lang="es" class="h-100">
 
@@ -16,30 +20,7 @@
 
 <body id="sobre-mi">
   <header class="container">
-    <nav class="navbar navbar-expand-md mb-4">
-      <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-          aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav me-auto mb-2 mb-md-0">
-            <li class="nav-item ">
-              <a class="nav-link px-4" href="index.html">Inicio</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link active px-4" href="sobre-mi.html">Sobre mi</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link px-4" href="proyectos.html">Mis proyectos</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link px-4" href="contacto.html">Contacto</a> <!--active indica cual es el menu activo-->
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+      <?php include_once "menu.php";?>
   </header>
   <main>
     <section id="bio" class="container">
@@ -47,7 +28,7 @@
         <div class="col-sm-8 col-12">
           <h1 class="pt-3 pb-5">Sobre mí</h1>
           <p class="pb-3">Docente frustrada de Educación Secundaria en Geografía</p>
-          <a href="contacto.html" class="btn btn-rojo">Enviar mensaje</a>
+          <a href="contacto.php" class="btn btn-rojo">Enviar mensaje</a>
         </div>
         <div class="col-sm-4 col-12">
           <img src="../portfolio/images/yop.png">
